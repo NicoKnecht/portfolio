5666b52eb33e658f74fe43147d933028c1e684e0
nicoKnecht <nicosanmarco.dev@gmail.com>
2023-02-22 02:26:58 -0300

    add meta tags and download cv and change some of the styles

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+
+
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -22,3 +25,5 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle('active');
     });
 });
+
+
